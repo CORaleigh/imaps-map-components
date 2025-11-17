8d4b2b07e55100fc626a29578872ce156d68cab7
justingreco <justin.greco@gmail.com>
2025-11-17 08:33:54 -0500

    hide tooltip on click

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/imaps-map-components">
       <App />
     </BrowserRouter>
   </StrictMode>
