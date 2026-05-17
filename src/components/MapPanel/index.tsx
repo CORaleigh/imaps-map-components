e4abebb55ef34c5a49ba52146350794e50959820
justingreco <justin.greco@gmail.com>
2026-05-17 14:59:50 -0400

    removed unneeded wrapper

--- src/components/MapPanel/index.tsx
+++ src/components/MapPanel/index.tsx
@@ -31,7 +31,9 @@ interface MapPanelProps {
   onCoordinateExpand: (
     event: HTMLArcgisExpandElement["arcgisPropertyChange"],
   ) => void;
-  onExpandChange: (event: HTMLArcgisExpandElement["arcgisPropertyChange"]) => void;
+  onExpandChange: (
+    event: HTMLArcgisExpandElement["arcgisPropertyChange"],
+  ) => void;
   onToolClose: () => void;
 }
 
@@ -81,131 +83,126 @@ const MapPanel: React.FC<MapPanelProps> = ({
   }, []);
 
   return (
-    <>
-      <arcgis-map
-        ref={mapElement}
-        onarcgisViewReadyChange={onMapReady}
-        onarcgisViewHold={onViewHold}
-      >
-        <arcgis-popup slot="popup"></arcgis-popup>
-        <arcgis-zoom slot="top-left"></arcgis-zoom>
-        <arcgis-home slot="top-left" goToOverride={onGoHome}></arcgis-home>
-        <arcgis-compass slot="top-left"></arcgis-compass>
-        <arcgis-track slot="top-left"></arcgis-track>
-        {/* <div slot="top-left">
+    <arcgis-map
+      ref={mapElement}
+      onarcgisViewReadyChange={onMapReady}
+      onarcgisViewHold={onViewHold}
+    >
+      <arcgis-popup slot="popup"></arcgis-popup>
+      <arcgis-zoom slot="top-left"></arcgis-zoom>
+      <arcgis-home slot="top-left" goToOverride={onGoHome}></arcgis-home>
+      <arcgis-compass slot="top-left"></arcgis-compass>
+      <arcgis-track slot="top-left"></arcgis-track>
+      {/* <div slot="top-left">
           <TipManager name="map" scale="s"></TipManager>
         </div> */}
-        <div slot="top-left" className={styles.customActions}>
-          <calcite-action-bar layout="vertical" expandDisabled>
-            <calcite-action
-              id="identify-action"
-              label="identify"
-              text={"identify"}
-              icon="information"
-              active={mapMode === "identify"}
-              onClick={() => onCustomActionClick("identify")}
-            ></calcite-action>
+      <div slot="top-left" className={styles.customActions}>
+        <calcite-action-bar layout="vertical" expandDisabled>
+          <calcite-action
+            id="identify-action"
+            label="identify"
+            text={"identify"}
+            icon="information"
+            active={mapMode === "identify"}
+            onClick={() => onCustomActionClick("identify")}
+          ></calcite-action>
 
-            <calcite-action
-              id="streetview-action"
-              label="streetview"
-              text={"streetview"}
-              icon="i360-view"
-              active={mapMode === "streetview"}
-              onClick={() => onCustomActionClick("streetview")}
-            ></calcite-action>
-          </calcite-action-bar>
-          <calcite-tooltip
-            reference-element="identify-action"
-            placement="right"
-          >
-            Identify
-          </calcite-tooltip>
-          <calcite-tooltip
-            reference-element="streetview-action"
-            placement="right"
-          >
-            Streetview
-          </calcite-tooltip>
-        </div>
-        <arcgis-expand
-          id="coordinate-action"
-          slot="bottom-left"
-          expandIcon="crosshair"
-          onarcgisPropertyChange={onCoordinateExpand}
-          mode="floating"
-          aria-label="Coodinate Conversion"
+          <calcite-action
+            id="streetview-action"
+            label="streetview"
+            text={"streetview"}
+            icon="i360-view"
+            active={mapMode === "streetview"}
+            onClick={() => onCustomActionClick("streetview")}
+          ></calcite-action>
+        </calcite-action-bar>
+        <calcite-tooltip reference-element="identify-action" placement="right">
+          Identify
+        </calcite-tooltip>
+        <calcite-tooltip
+          reference-element="streetview-action"
+          placement="right"
         >
-          {mapElement.current?.map && (
-            <CoodinateConversion
+          Streetview
+        </calcite-tooltip>
+      </div>
+      <arcgis-expand
+        id="coordinate-action"
+        slot="bottom-left"
+        expandIcon="crosshair"
+        onarcgisPropertyChange={onCoordinateExpand}
+        mode="floating"
+        aria-label="Coodinate Conversion"
+      >
+        {mapElement.current?.map && (
+          <CoodinateConversion
+            mapElement={mapElement}
+            isOpen={coordinateConversionOpen}
+          ></CoodinateConversion>
+        )}
+      </arcgis-expand>
+
+      <arcgis-scale-bar slot="bottom-left"></arcgis-scale-bar>
+      <arcgis-expand
+        slot="bottom-right"
+        expandIcon="arrow-up-left"
+        collapseIcon="arrow-down-right"
+        id="overview-action"
+        mode="floating"
+        onarcgisPropertyChange={onExpandChange}
+        aria-label="Overview Map"
+      >
+        <OverviewMap mapElement={mapElement}></OverviewMap>
+      </arcgis-expand>
+
+      <div slot="top-right">
+        {openedTools.includes("select") && (
+          <Suspense fallback={null}>
+            <PropertySelect
               mapElement={mapElement}
-              isOpen={coordinateConversionOpen}
-            ></CoodinateConversion>
-          )}
-        </arcgis-expand>
-
-        <arcgis-scale-bar slot="bottom-left"></arcgis-scale-bar>
-        <arcgis-expand
-          slot="bottom-right"
-          expandIcon="arrow-up-left"
-          collapseIcon="arrow-down-right"
-          id="overview-action"
-          mode="floating"
-          onarcgisPropertyChange={onExpandChange}
-          aria-label="Overview Map"
-        >
-          <OverviewMap mapElement={mapElement}></OverviewMap>
-        </arcgis-expand>
-
-        <div slot="top-right">
-          {openedTools.includes("select") && (
-            <Suspense fallback={null}>
-              <PropertySelect
-                mapElement={mapElement}
-                onToolClose={onToolClose}
-                closed={activeTool !== "select"}
-              ></PropertySelect>
-            </Suspense>
-          )}
-          {openedTools.includes("location") && (
-            <Suspense fallback={null}>
-              <LocationSearch
-                mapElement={mapElement}
-                onToolClose={onToolClose}
-                closed={activeTool !== "location"}
-              />
-            </Suspense>
-          )}
-          {openedTools.includes("measure") && (
-            <Suspense fallback={null}>
-              <Measure
-                onToolClose={onToolClose}
-                mapElement={mapElement}
-                closed={activeTool !== "measure"}
-              ></Measure>
-            </Suspense>
-          )}
-          {openedTools.includes("sketch") && (
-            <Suspense fallback={null}>
-              <Sketch
-                onToolClose={onToolClose}
-                closed={activeTool !== "sketch"}
-                mapElement={mapElement}
-              ></Sketch>
-            </Suspense>
-          )}
-          {openedTools.includes("print") && (
-            <Suspense fallback={null}>
-              <Print
-                mapElement={mapElement}
-                onToolClose={onToolClose}
-                closed={activeTool !== "print"}
-              ></Print>
-            </Suspense>
-          )}
-        </div>
-      </arcgis-map>
-    </>
+              onToolClose={onToolClose}
+              closed={activeTool !== "select"}
+            ></PropertySelect>
+          </Suspense>
+        )}
+        {openedTools.includes("location") && (
+          <Suspense fallback={null}>
+            <LocationSearch
+              mapElement={mapElement}
+              onToolClose={onToolClose}
+              closed={activeTool !== "location"}
+            />
+          </Suspense>
+        )}
+        {openedTools.includes("measure") && (
+          <Suspense fallback={null}>
+            <Measure
+              onToolClose={onToolClose}
+              mapElement={mapElement}
+              closed={activeTool !== "measure"}
+            ></Measure>
+          </Suspense>
+        )}
+        {openedTools.includes("sketch") && (
+          <Suspense fallback={null}>
+            <Sketch
+              onToolClose={onToolClose}
+              closed={activeTool !== "sketch"}
+              mapElement={mapElement}
+            ></Sketch>
+          </Suspense>
+        )}
+        {openedTools.includes("print") && (
+          <Suspense fallback={null}>
+            <Print
+              mapElement={mapElement}
+              onToolClose={onToolClose}
+              closed={activeTool !== "print"}
+            ></Print>
+          </Suspense>
+        )}
+      </div>
+    </arcgis-map>
   );
 };
 
